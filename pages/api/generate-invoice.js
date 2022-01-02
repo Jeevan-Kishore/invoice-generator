c3f8a8af87825b3059ccb525e2d4d7e80f093f7e
jeevan-kishore <kishor080@gmail.com>
2022-01-02 19:08:12 +0530

    trial generate invoice route

---
+++ pages/api/generate-invoice.js
@@ -0,0 +1,13 @@
+const path = require("path");
+const ejs = require("ejs");
+const pdf = require("html-pdf");
+
+export default function handler(req, res) {
+    if (req.method === 'POST') {
+        ejs.renderFile(path.join(__dirname, './views/', "invoice-template.ejs"), {}, (err, data) => {
+            res.send(data);
+        });
+    } else {
+        // Handle any other HTTP method
+    }
+}
