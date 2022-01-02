473bb61b0a8303cf57b6fd550c56a03aa440b326
jeevan-kishore <kishor080@gmail.com>
2022-01-02 21:13:59 +0530

    replace pdf generator

--- pages/api/generate-invoice.js
+++ pages/api/generate-invoice.js
@@ -1,21 +1,20 @@
 const ejs = require("ejs");
-const pdf = require("html-pdf");
+const pdf = require('html-pdf-node');
 const { join } = require('path')
 
+
+const pdfOptions = { format: 'A4' };
 export default function handler(req, res) {
     if (req.method === 'POST') {
         ejs.renderFile(join(process.cwd(), 'views', 'invoice-template.ejs'), {}, (err, data) => {
             if (err) {
                 res.send(err);
             }
-            /*res.setHeader("Content-Type","application/pdf");*/
-            pdf.create(data).toFile("report.pdf", function (err, data) {
-                if (err) {
-                    res.send(err);
-                } else {
-                    console.log(res.filename);
-                    res.send("File created successfully");
-                }
+            const file = { content: data };
+            res.setHeader("Content-Type","application/pdf");
+            pdf.generatePdf(file, pdfOptions).then(pdfBuffer => {
+                console.log("PDF Buffer:-", pdfBuffer);
+                res.send(pdfBuffer)
             });
         });
     } else {
