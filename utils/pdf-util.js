add9b8421021b6418fbf4aeff9e279a6d19d94f4
jeevan-kishore <kishor080@gmail.com>
2022-01-02 23:31:23 +0530

    Modify home screen

--- utils/pdf-util.js
+++ utils/pdf-util.js
@@ -7,7 +7,7 @@ module.exports
 async function generatePdf(file, options, callback) {
 
     let browser = await puppeteer.launch({
-        args: [...chrome.args, '--hide-scrollbars', '--disable-web-security'],
+        args: [...chrome.args, '--hide-scrollbars', '--disable-web-security', '--no-sandbox', '--disable-setuid-sandbox'],
         defaultViewport: chrome.defaultViewport,
         executablePath: await chrome.executablePath,
         headless: true,
@@ -35,37 +35,31 @@ async function generatePdf(file, options, callback) {
         });
     }
 
-    console.log(" DEBUG: ", "---------------------------> HERE");
-
     return Promise.props(page.pdf(options))
         .then(async function(data) {
             await browser.close();
-            console.log(" DEBUG: ", "---------------------------> THERE");
             return Buffer.from(Object.values(data));
         }).asCallback(callback);
 }
 
 async function generatePdfs(files, options, callback) {
-    // we are using headless mode
-    let args = [
-        '--no-sandbox',
-        '--disable-setuid-sandbox',
-    ];
-    if(options.args) {
-        args = options.args;
-        delete options.args;
-    }
-    const browser = await puppeteer.launch({
-        args: args
+    let browser = await puppeteer.launch({
+        args: [...chrome.args, '--hide-scrollbars', '--disable-web-security', '--no-sandbox', '--disable-setuid-sandbox'],
+        defaultViewport: chrome.defaultViewport,
+        executablePath: await chrome.executablePath,
+        headless: true,
+        ignoreHTTPSErrors: true,
     });
+
     let pdfs = [];
     const page = await browser.newPage();
-    for(let file of files) {
-        if(file.content) {
-            data = await inlineCss(file.content, {url:"/"})
+    let data;
+    for (let file of files) {
+        if (file.content) {
+            data = await inlineCss(file.content, {url: "/"})
             console.log("Compiling the template with handlebars")
             // we have compile our code with handlebars
-            const template = hb.compile(data, { strict: true });
+            const template = hb.compile(data, {strict: true});
             const result = template(data);
             const html = result;
             // We set the page content as the generated html by handlebars
