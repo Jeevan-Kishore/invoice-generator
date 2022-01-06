25614b82afcaf53377f4e467527b9d9a06253f9e
jeevan-kishore <kishor080@gmail.com>
2022-01-06 13:38:09 +0530

    feat(data): Add data collections

--- pages/api/generate-invoice.js
+++ pages/api/generate-invoice.js
@@ -1,24 +1,27 @@
 const ejs = require("ejs");
 const pdf = require("../../utils/pdf-util");
-const { join } = require('path')
-
+const { join } = require("path");
 
 export default function handler(req, res) {
-    if (req.method === 'POST') {
-        const { body } = req;
-        const postData = JSON.parse(body);
-        ejs.renderFile(join(process.cwd(), 'utils', 'invoice-template.ejs'), postData, (err, data) => {
-            if (err) {
-                res.send(err);
-            }
-            const file = { content: data };
+  if (req.method === "POST") {
+    const { body } = req;
+    const postData = JSON.parse(body);
+    ejs.renderFile(
+      join(process.cwd(), "utils", "invoice-template.ejs"),
+      { ...postData },
+      (err, data) => {
+        if (err) {
+          res.send(err);
+        }
+        const file = { content: data };
 
-            pdf.generatePdf(file).then(pdfBuffer => {
-                res.setHeader("Content-Type","application/pdf");
-                res.send(pdfBuffer);
-            });
+        pdf.generatePdf(file).then((pdfBuffer) => {
+          res.setHeader("Content-Type", "application/pdf");
+          res.send(pdfBuffer);
         });
-    } else {
-        res.status(400).json({ error: 'Unsupported method' });
-    }
+      }
+    );
+  } else {
+    res.status(400).json({ error: "Unsupported method" });
+  }
 }
--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,16 @@
 import React, { useState } from "react";
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import Head from "next/head";
+import Image from "next/image";
+import styles from "../styles/Home.module.css";
 
 const defaultUserData = {
   name: "",
   address: "",
   amount: "",
-  duration: ""
+  duration: "",
+  mobile: "",
+  userId: "",
+  accountNumber: "",
 };
 
 export default function Home() {
@@ -15,17 +18,17 @@ export default function Home() {
 
   const onChangeHandler = (e, target) => {
     const value = e.target.value;
-    setUserData({...userData, ...{[target]: value}});
-  }
+    setUserData({ ...userData, ...{ [target]: value } });
+  };
 
   const onClickHandler = async () => {
     const response = await fetch("/api/generate-invoice", {
-      method: 'POST',
-      body: JSON.stringify({...userData})
+      method: "POST",
+      body: JSON.stringify({ ...userData }),
     });
     const blob = await response.blob();
     // It is necessary to create a new blob object with mime-type explicitly set for all browsers except Chrome, but it works for Chrome too.
-    const newBlob = new Blob([blob], { type: 'application/pdf' });
+    const newBlob = new Blob([blob], { type: "application/pdf" });
 
     // MS Edge and IE don't allow using a blob object directly as link href, instead it is necessary to use msSaveOrOpenBlob
     if (window.navigator && window.navigator.msSaveOrOpenBlob) {
@@ -34,13 +37,15 @@ export default function Home() {
       // For other browsers: create a link pointing to the ObjectURL containing the blob.
       const objUrl = window.URL.createObjectURL(newBlob);
 
-      let link = document.createElement('a');
+      let link = document.createElement("a");
       link.href = objUrl;
       link.download = `ACT-receipt-${new Date().getTime()}`;
       link.click();
 
       // For Firefox it is necessary to delay revoking the ObjectURL.
-      setTimeout(() => { window.URL.revokeObjectURL(objUrl); }, 250);
+      setTimeout(() => {
+        window.URL.revokeObjectURL(objUrl);
+      }, 250);
     }
   };
 
@@ -53,23 +58,25 @@ export default function Home() {
       </Head>
 
       <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to invoice generator
-        </h1>
+        <h1 className={styles.title}>Welcome to invoice generator</h1>
 
-        <p className={styles.description}>
-          For education purposes ONLY!
-        </p>
+        <p className={styles.description}>For education purposes ONLY!</p>
 
         <div className={styles.grid}>
-          {
-            Object.keys(defaultUserData).map((item, index) => <div key={`${item}-${index}`} className={styles.card}>
+          {Object.keys(defaultUserData).map((item, index) => (
+            <div key={`${item}-${index}`} className={styles.card}>
               <h2>{item}:</h2>
-              <textarea type="text" value={userData[item]} onChange={(e) => onChangeHandler(e, item)}/>
-            </div>)
-          }
+              <textarea
+                type="text"
+                value={userData[item]}
+                onChange={(e) => onChangeHandler(e, item)}
+              />
+            </div>
+          ))}
         </div>
-        <button className={styles.generateButton} onClick={onClickHandler}>Generate Invoice</button>
+        <button className={styles.generateButton} onClick={onClickHandler}>
+          Generate Invoice
+        </button>
       </main>
 
       <footer className={styles.footer}>
@@ -78,12 +85,12 @@ export default function Home() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Powered by{' '}
+          Powered by{" "}
           <span className={styles.logo}>
             <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
           </span>
         </a>
       </footer>
     </div>
-  )
+  );
 }
